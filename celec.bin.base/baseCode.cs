159991e6949520813932ef145d9e581fecb386c4
LEONEL.MUÑOZ <leonel.munoz@celec.gob.ec>
2017-09-29 09:54:40 -0500

    commit

--- celec.bin.base/baseCode.cs
+++ celec.bin.base/baseCode.cs
@@ -11,6 +11,10 @@ namespace celec.bin.@base
         public String GetConn(String _userName, String _userPassword) {
             return BuildConnectionString(_userName, _userPassword);
         }
+        public String GetConn(String _userName, String _userPassword, String _dataSource)
+        {
+            return BuildConnectionString(_userName, _userPassword,_dataSource);
+        }
         private static String BuildConnectionString(String userName, String userPassword) {
 
             ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["Mantenimiento_ConnectionString"];
@@ -22,6 +26,22 @@ namespace celec.bin.@base
                 builder.Password = userPassword;
 
                 return builder.ConnectionString.ToString() ;
+            }
+            return String.Empty;
+        }
+        private static String BuildConnectionString(String userName, String userPassword, String dataSource)
+        {
+
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["Mantenimiento_ConnectionString"];
+            if (settings != null)
+            {
+                String _ConnectionString = settings.ConnectionString;
+                OracleConnectionStringBuilder builder = new OracleConnectionStringBuilder(_ConnectionString);
+                builder.UserID = userName;
+                builder.Password = userPassword;
+                builder.DataSource = dataSource;
+
+                return builder.ConnectionString.ToString();
             }
             return String.Empty;
         }
